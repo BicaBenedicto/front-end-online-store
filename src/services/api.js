d94af4b399c59b2a05198b473ef3ca258d9251be
Danilo Uehara <daniloyuno@hotmail.com>
2021-10-25 15:52:11 -0300

    arrumando erro lint

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ export async function getCategories() {
   return jsonCategories;
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const fetchProducts = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const jsonProducts = await fetchProducts.json();
