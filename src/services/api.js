b19bc820dc4a07e3e887ea857c1971c92cfe9e73
Danilo Uehara <daniloyuno@hotmail.com>
2021-10-25 15:43:29 -0300

    requisito01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fecthCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const jsonCategories = await fecthCategories.json();
+  return jsonCategories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query ) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const fetchProducts = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const jsonProducts = await fetchProducts.json();
+  return jsonProducts;
 }
