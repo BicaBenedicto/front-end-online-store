e1b50de7160e0cd0b394a54b3dcefd5954a03f4b
JoaoVMont <joaovmt1905@gmail.com>
2021-10-26 14:54:14 -0300

    Arrumando conflitos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import CartPage from './components/Cart/CartPage/CartPage';
 
-class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/cart" component={ CartPage } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
-
-export default App;
