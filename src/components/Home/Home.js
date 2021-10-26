2df20ef8477b4b9152cb41d6edfaf660f51ed979
JoaoVMont <joaovmt1905@gmail.com>
2021-10-26 14:32:30 -0300

    Arrumando arquivos quebrados

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,14 @@ import Categories from '../Categories/Categories';
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <>
         <header>
           <h1 data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h1>
         </header>
         <Categories />
-
-      </div>
+      </>
     );
   }
 }
